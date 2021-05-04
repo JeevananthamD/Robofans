0f6765b309d4d46006d8cc3cb065cd1a1be6833f
Jeevanantham <jeevanantham97d@gmail.com>
2021-05-04 20:41:50 +0530

    For hosting

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Robofans
+![image](https://user-images.githubusercontent.com/71116484/116862931-20429780-ac23-11eb-9355-c3ca6e8d830e.png)
+
+![image](https://user-images.githubusercontent.com/71116484/116862958-2df81d00-ac23-11eb-96b1-0dc0ff8a00b1.png)
---
+++ src/containers/App.js
@@ -0,0 +1,76 @@
+import React,{ Component } from 'react';
+import CardList from '../components/CardList';
+import SearchBox from '../components/SearchBox';
+import Scroll from '../components/Scroll';
+import "tachyons";
+import './App.css';
+import Popup from '../components/Popup';
+
+
+class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      robots: [],
+      search_field: "",
+      popup: false
+    }
+    this.popupImg = "";
+    this.filtered_robots =[]
+    
+  }
+
+  async componentDidMount() {
+    await fetch("https://jsonplaceholder.typicode.com/users")
+    .then(response => response.json())
+    .then(users => {
+      this.setState({robots: users});
+    });
+  }
+  
+  onSearchChange = (event) => {
+    this.setState({search_field: event.target.value});
+  }
+
+  popupFunction = (event) => {
+    if(this.state.popup) {
+      console.log("close");
+      this.setState({popup: false});
+    }
+    else {
+      this.popupData = {
+       imgsrc: event.target.src,
+       details: this.filtered_robots[event.target.id]
+      }
+      console.log(this.popupData.details);
+      this.setState({popup: true});
+    }
+  }
+
+  render() {
+
+    const {robots, search_field, popup} = this.state;
+    this.filtered_robots = robots.filter(robot => robot.name.toLowerCase().includes(search_field.toLowerCase()));
+
+    return (
+      <React.Fragment>
+        {popup?<Popup popup={this.popupFunction} imgsrc={this.popupData.imgsrc} details={this.popupData.details}/>:""}
+        <div className="tc">
+          <h1>Robofans</h1>
+          <SearchBox searchChange={this.onSearchChange}/>
+          <Scroll>
+            { (!this.filtered_robots.length)?
+                <h2>No profile match your search</h2>
+                :
+                <CardList robots={this.filtered_robots} popup={this.popupFunction}/>
+            }
+          </Scroll>
+        </div>
+      </React.Fragment>
+      
+    );
+  }
+}
+
+export default App;
